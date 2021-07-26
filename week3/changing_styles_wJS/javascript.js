839bc260c79fbcb448c38bd98073e3e6db1a9cb2
Fernando140 <fernando.vazquez@bryanuniversity.edu>
2021-07-25 20:52:17 -0500

    Changing styles and classes using JS

---
+++ week3/changing_styles_wJS/javascript.js
@@ -0,0 +1,11 @@
+for (var i = 0; i < 5; i++) {
+    var header2 = document.createElement("h2")
+    header2.classList.add("border")
+    header2.textContent = "Hello World"
+    document.body.appendChild(header2)
+    header2.style.fontSize = "20px"
+    header2.style.fontWeight = "lighter"
+    header2.style.fontFamily = "sans-serif"
+    header2.style.color = "cornflowerblue"
+    
+}
